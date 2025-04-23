a1602290a3440ccfd0427652f7299e6a01a0b956
syed <syed.feroz@epiplexai>
2025-04-23 15:51:59 +0530

    aadding comment for testing

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def main():
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
-
+# use port 8080 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
